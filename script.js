50ae8fd767b2ab383ad2116e3fd509a78e7538ea
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-02-22 21:37:44 -0300

    rescrevendo utilizando async await

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-var consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-.then(response => response.json())
-.then(r => {
-    if (r.erro) {
-        throw Error('Esse cep não existe');
-    }else {
-        console.log(r);
-    }
-})
-.catch(e => console.log(e))
-.finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco() {
+    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
+    var consultaCepConvertida = await consultaCep.json();
+    console.log(consultaCepConvertida);
+}
 
-console.log(consultaCep);
+buscaEndereco();
