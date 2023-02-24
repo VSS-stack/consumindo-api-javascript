8bc854be51f81c2d0810bb4f12e148889da3b6f6
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-02-23 21:49:19 -0300

    implementando try catch

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 async function buscaEndereco() {
-    var consultaCep = await fetch('https://viacep.com.br/ws/01001000/json/');
-    var consultaCepConvertida = await consultaCep.json();
-    console.log(consultaCepConvertida);
+    try {
+        var consultaCep = await fetch('https://viacep.com.br/ws/01001005/json/');
+        var consultaCepConvertida = await consultaCep.json();
+        if (consultaCepConvertida.erro) {
+            throw Error('CEP não existe!');
+        }
+        console.log(consultaCepConvertida);
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 buscaEndereco();
