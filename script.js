490939c4d00dc8512de73238c7a7aad3a100a62b
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-02-23 22:09:19 -0300

    testando o método Promise.all

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-async function buscaEndereco() {
+async function buscaEndereco(cep) {
     try {
-        var consultaCep = await fetch('https://viacep.com.br/ws/01001005/json/');
+        var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCepConvertida = await consultaCep.json();
         if (consultaCepConvertida.erro) {
             throw Error('CEP não existe!');
         }
         console.log(consultaCepConvertida);
+        return consultaCepConvertida;
     } catch (error) {
         console.log(error);
     }
     
 }
 
-buscaEndereco();
+let ceps = ['01001000', '13453842'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
