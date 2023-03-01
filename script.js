f081575b44db238f1eec6849f73c016852d6bc23
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-03-01 20:59:33 -0300

    adicionando mensagem de erro para o usuário

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCepConvertida = await consultaCep.json();
@@ -17,6 +19,7 @@ async function buscaEndereco(cep) {
         console.log(consultaCepConvertida);
         return consultaCepConvertida;
     } catch (error) {
+        mensagemErro.innerHTML = '<p>CEP inválido. Tente novamente</p>'
         console.log(error);
     }
     
